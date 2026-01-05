210dddaef5900c82019375e012ae45f6d9dac399
ProneirJames <a80753511@gmail.com>
2026-01-05 20:26:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker: Installed");
+const CACHE_NAME = 'my-app-v1';
+const urlsToCache = ['/index.html', '/index.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker: Activated");
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
-
-// Optional: listen for fetch events (not caching)
-self.addEventListener("fetch", () => {});
